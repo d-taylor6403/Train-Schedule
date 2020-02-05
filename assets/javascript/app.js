8a90e7e5287ea5abff587bb206651ace548efaae
dannitay <dannielletaylor@outlook.com>
2020-02-05 17:30:34 -0600

    adding click event for button and time conversion

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -19,4 +19,28 @@ var firebaseConfig = {
   //-------------Functions-------------------
 
   //On submit button click
-  
+
+  $("#train-submit").on("click", function(){
+      event.preventDefault();
+
+      var trainName = $("#train-name").val().trim();
+      var trainDestination = $("#train-destination").val().trim();
+      var trainFirstTime = $("#train-time").val().trim();
+      var trainFrequency = $("#train-frequency").val().trim();
+
+      console.log("First Train Time: " + trainFirstTime);
+
+      //Convert First Train Time to happen 1 year in the past
+      var firstTimeConverted = moment(trainFirstTime, "HH:mm").subtract(1, "years");
+      console.log("First Time Converted: " + firstTimeConverted);
+      //Variable for the difference between current time and the time in the past
+      var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+      console.log("Time Difference: " +diffTime);
+      //Remaining time in a variable
+      var timeRemaining = diffTime % trainFrequency;
+      console.log("Remaining Time: " + timeRemaining);
+      var timeTilTrain = trainFrequency - timeRemaining;
+      console.log("Time Until Next Train: " + timeTilTrain);
+
+
+  })
